ff34b0816d8d545c8f8f90777c538fd7d0ef4c54
Thr0TT1e <7hr0771e@gmail.com>
2021-11-10 21:48:29 +0300

    project initial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,106 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <div class="divForm">
+      <form action="">
+        <input
+            :value="titlePost"
+            class="inputForm"
+            type="text"
+            placeholder="Название"
+        >
+        <input
+            :value="bodyPost"
+            class="inputForm"
+            type="text"
+            placeholder="Описание"
+        >
+        <button class="btn" @click="createPost">Добавить</button>
+      </form>
+    </div>
+
+    <div
+        class="post"
+        v-for="post in posts"
+        key="post.id"
+    >
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  data() {
+    return {
+      titlePost: '',
+      bodyPost: '',
+      posts: [
+        {
+          id: 1,
+          title: 'Индексаторы (Indexers) в JavaScript',
+          body: 'В некоторых современных объектно-ориентированных языках есть понятие идексаторов – свойств, позволяющих работать с экземпляром класса как с массивом, используя [] нотацию. В этой статье я хочу продемонстрировать как это сделать на современном JavaScript.'
+        },
+        {
+          id: 2,
+          title: 'Базовые задачи на Promise в JavaScript',
+          body: 'Promise - специальный объект, который хранит своё состояние и используется для обработки результатов асинхронных операций.'
+        },
+        {
+          id: 3,
+          title: 'Стилизация однофайловых Vue компонентов',
+          body: 'Если у вас есть опыт написания однофайловых Vue компонентов, вы, вероятно, сталкивались с написанием CSS в своем компоненте. Они позволяют разработчикам группировать код более логическими способами, а не разбивать компоненты по используемому языку (HTML, CSS или JavaScript).'
+        },
+        {
+          id: 4,
+          title: 'Создание веб-компонентов с помощью Vue 3.2',
+          body: 'Вы когда-нибудь работали над несколькими проектами и хотели иметь набор настраиваемых компонентов, которые можно было бы использовать во всех из них?'
+        },
+      ]
     }
-  }
+  },
+
+  methods: {},
 }
+</script>
+
+<style lang="sass">
+body
+  width: 80%
+  border: 0
+  margin: 0 auto
+  box-sizing: border-box
+
+.wrapper
+  width: 100%
+  padding: 20px
+
+.divForm
+  width: 100%
+  form
+    box-sizing: border-box
+    display: grid
+    grid-gap: 10px
+
+.post
+  margin-top: 15px
+  padding: 15px
+  border: 2px solid teal
+
+.inputForm
+  border: 1px solid teal
+  padding: 10px 10px
+
+.btn
+  width: 30%
+  padding: 10px 15px
+  margin-top: 5px
+  border: 1px solid teal
+  background: none
+  color: teal
+  &:hover
+    background: teal
+    color: white
 </style>
